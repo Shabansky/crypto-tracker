e242ebedcc6ce3d43354d19d4d9c18054a0e9a0f
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-03-08 13:45:36 +0200

    Add endpoint validation tests

--- tests/Feature/EndpointsTest.php
+++ tests/Feature/EndpointsTest.php
@@ -18,6 +18,96 @@ class EndpointsTest extends TestCase
         $response->assertContent('New Subscription');
     }
 
+    /**
+     * This test holds the entirety of validations that can
+     * be encountered in the three endpoints. Thus specific data
+     * validations will not be repeated for other endpoints.
+     * 
+     * The only specific tests are the ones testing for request
+     * parameters existence as they are integral to the endpoint's
+     * functioning.
+     */
+    public function test_post_validations_fail(): void
+    {
+        /* Missing email parameter */
+        $response = $this->postJson('api/subscription', [
+            'threshold' => 100.00,
+            'timeframe' => 1,
+        ]);
+
+        $response->assertStatus(400);
+
+        /* Missing threshold parameter */
+        $response = $this->postJson('api/subscription', [
+            'email' => 'test@example.com',
+            'timeframe' => 1,
+        ]);
+
+        $response->assertStatus(400);
+
+        /* Missing timeframe parameter */
+        $response = $this->postJson('api/subscription', [
+            'email' => 'test@example.com',
+            'threshold' => 100.00,
+        ]);
+
+        $response->assertStatus(400);
+
+        /* Email not proper mail */
+        $response = $this->postJson('api/subscription', [
+            'email' => 'test.example.com',
+            'threshold' => 100.00,
+            'timeframe' => 1,
+        ]);
+
+        $response->assertStatus(400);
+
+        /* Threshold negative */
+        $response = $this->postJson('api/subscription', [
+            'email' => 'test.example.com',
+            'threshold' => -20.0,
+            'timeframe' => 1,
+        ]);
+
+        $response->assertStatus(400);
+
+        /* Threshold non-numeric */
+        $response = $this->postJson('api/subscription', [
+            'email' => 'test.example.com',
+            'threshold' => "some percentage",
+            'timeframe' => 1,
+        ]);
+
+        $response->assertStatus(400);
+
+        /* Threshold precision different than 0,2 */
+        $response = $this->postJson('api/subscription', [
+            'email' => 'test.example.com',
+            'threshold' => 100.000012,
+            'timeframe' => 1,
+        ]);
+
+        $response->assertStatus(400);
+
+        /* Timeframe non-numeric */
+        $response = $this->postJson('api/subscription', [
+            'email' => 'test.example.com',
+            'threshold' => 100.00,
+            'timeframe' => 'one',
+        ]);
+
+        $response->assertStatus(400);
+
+        /* Timeframe not in allowed list (1, 6, 24) */
+        $response = $this->postJson('api/subscription', [
+            'email' => 'test.example.com',
+            'threshold' => 100.00,
+            'timeframe' => 2,
+        ]);
+
+        $response->assertStatus(400);
+    }
+
     public function test_patch_success(): void
     {
         /* With both threshold and timeframe defined */
@@ -57,6 +147,17 @@ class EndpointsTest extends TestCase
         $response->assertContent('Edit Existing Subscription');
     }
 
+    public function test_patch_validations_fail()
+    {
+        /* Missing email parameter */
+        $response = $this->postJson('api/subscription', [
+            'threshold' => 100.00,
+            'timeframe' => 1,
+        ]);
+
+        $response->assertStatus(400);
+    }
+
     public function test_delete_success(): void
     {
         $response = $this->deleteJson('api/subscription', [
@@ -65,5 +166,16 @@ class EndpointsTest extends TestCase
 
         $response->assertStatus(200);
         $response->assertContent('Delete Existing Subscription');
+    }
+
+    public function test_delete_validations_fail()
+    {
+        /* Missing email parameter */
+        $response = $this->postJson('api/subscription', [
+            'threshold' => 100.00,
+            'timeframe' => 1,
+        ]);
+
+        $response->assertStatus(400);
     }
 }
