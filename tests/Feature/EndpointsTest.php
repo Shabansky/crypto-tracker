38b9caad4a6f1ba36bd04ae6ad8d73ef49aedadd
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-03-09 20:58:49 +0200

    Refresh database on EndpointsTest execution

--- tests/Feature/EndpointsTest.php
+++ tests/Feature/EndpointsTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class EndpointsTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_post_success(): void
     {
         $response = $this->postJson('api/subscription', [
