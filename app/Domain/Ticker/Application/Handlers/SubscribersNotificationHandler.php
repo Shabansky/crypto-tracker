14601ec137127c609349748f82cce9bf5e185a0a
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-03-10 11:46:28 +0200

    Retrieve subscriptions to mail per timeframe

--- app/Domain/Ticker/Application/Handlers/SubscribersNotificationHandler.php
+++ app/Domain/Ticker/Application/Handlers/SubscribersNotificationHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Domain\Ticker\Application\Handlers;
 
+use App\Domain\Subscription\Domain\Subscription;
 use App\Domain\Ticker\Domain\Models\HourlyTicker;
 use App\Domain\Ticker\Domain\Repositories\HourlyTickerRepository;
 use App\Domain\Ticker\Infrastructure\PriceDifferenceGenerator;
@@ -14,6 +15,43 @@ use Illuminate\Support\Facades\Log;
 class SubscribersNotificationHandler
 {
     public function handle(TickerProviderInterface $provider)
+    {
+        $this->addTickerFromProvider($provider);
+
+        //Get current Tickers (up to 25 - Current one plus the last 24 hours.
+        //Reason for this is we need the intervals between these hours)
+        $currentTickers = HourlyTickerRepository::getLatestTickersPrices(25);
+
+        foreach (PriceDifferenceGenerator::generate($currentTickers) as $priceDifferenceDto) {
+            $emailsToNotify = $this->getEmailsToNotifyByTimeframe($priceDifferenceDto);
+            self::logPriceDifference($priceDifferenceDto);
+        }
+    }
+
+    protected static function logPriceDifference(PriceDifferenceDto $priceDifferenceDto)
+    {
+        $logMessage = sprintf(
+            "Timeframe (hours): %s, Latest Price : %s, Initial Price : %s, Price Difference : %s, Percentage : %s",
+            $priceDifferenceDto->timeframe,
+            $priceDifferenceDto->latestPrice,
+            $priceDifferenceDto->initialPrice,
+            $priceDifferenceDto->getPriceDifferenceAbs(),
+            $priceDifferenceDto->getPercentageDifference()
+        );
+        Log::notice($logMessage);
+    }
+
+    protected function getEmailsToNotifyByTimeframe(
+        PriceDifferenceDto $priceDifferenceDto
+    ): array {
+        return Subscription::select('email')
+            ->where('timeframe', $priceDifferenceDto->timeframe)
+            ->where('threshold', '<=', $priceDifferenceDto->getPercentageDifference())
+            ->pluck('email')
+            ->toArray();
+    }
+
+    protected function addTickerFromProvider(TickerProviderInterface $provider)
     {
         //Try to get data from provider
         try {
@@ -28,26 +66,5 @@ class SubscribersNotificationHandler
             'price' => $response->price,
             'time' => $response->time
         ]);
-
-        //Get current Tickers (up to 25 - Current one plus the last 24 hours.
-        //Reason for this is we need the intervals between these hours)
-        $currentTickers = HourlyTickerRepository::getLatestTickersPrices(25);
-
-        foreach (PriceDifferenceGenerator::generate($currentTickers) as $priceDifferenceDto) {
-            self::logPriceDifference($priceDifferenceDto);
-        }
-    }
-
-    private static function logPriceDifference(PriceDifferenceDto $priceDifferenceDto)
-    {
-        $logMessage = sprintf(
-            "Timeframe (hours): %s, Latest Price : %s, Initial Price : %s, Price Difference : %s, Percentage : %s",
-            $priceDifferenceDto->timeframe,
-            $priceDifferenceDto->latestPrice,
-            $priceDifferenceDto->initialPrice,
-            $priceDifferenceDto->getPriceDifferenceAbs(),
-            $priceDifferenceDto->getPercentageDifference()
-        );
-        Log::notice($logMessage);
     }
 }
